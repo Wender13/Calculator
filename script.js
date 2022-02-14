9b3c9071f24a41d8d56afe25d6d13c5c73141ccb
Wender <w3nd3r.jr62@gmail.com>
2022-02-14 14:35:23 -0300

    Calculator works. New tools and corrections soon.

---
+++ script.js
@@ -0,0 +1,114 @@
+// Variables
+
+let DarkMode = false
+let DisplayValue = document.getElementById('Screen')
+let ActualNumericValue = '0'
+let ActualOperator = 'none'
+let FirstValue = '0'
+let SecondValue = '0'
+let result = '0'
+
+// Operators' functions
+
+function add(a,b){
+    result = Number(a) + Number(b)
+    DisplayValue.textContent = result
+}
+
+function subtract(a,b){
+    result = Number(a) - Number(b)
+    DisplayValue.textContent = result
+}
+
+function multiply(a,b){
+    result =  Number(a) * Number(b)
+    DisplayValue.textContent = result
+}
+
+function divide(a,b){
+    result = Number(a) / Number(b)
+    DisplayValue.textContent = result
+}
+
+
+// DOM functions 
+
+function RemoveLastNumber() {
+    if(ActualNumericValue != 0 && ActualNumericValue.length > 1) {
+        const Array = ActualNumericValue.split('')
+        Array.pop()
+        ActualNumericValue = Array.join('')
+        DisplayValue.textContent = ActualNumericValue
+    } else if (ActualNumericValue.length == 1 && ActualNumericValue != 0){
+        ActualNumericValue = '0'
+        DisplayValue.textContent = ActualNumericValue
+    }
+}
+
+// Events
+
+window.addEventListener('load', function () {
+    DisplayValue.textContent = ActualNumericValue
+})
+
+function AddValue(value) {
+    if (ActualNumericValue == 0) {
+        ActualNumericValue = value
+    } else if (ActualNumericValue != 0) {
+        ActualNumericValue = `${ActualNumericValue}` + `${value}`
+    }
+    DisplayValue.textContent = ActualNumericValue
+}
+
+function ClearAll(){
+    ActualNumericValue = '0'
+    DisplayValue.textContent = ActualNumericValue
+    ActualOperator = 'none'
+    FirstValue = '0'
+    SecondValue = '0'
+    result = '0'
+}
+
+function SetOperator(operator) {
+    if (FirstValue != 0 && ActualNumericValue != 0) {
+        Result()
+    } else {
+        ActualOperator = operator
+        FirstValue = ActualNumericValue
+        ActualNumericValue = '0'
+        DisplayValue.textContent = ActualNumericValue
+    }
+}
+
+function Result() {
+    SecondValue = ActualNumericValue
+    ActualNumericValue = '0'
+    switch (ActualOperator) {
+        case '+':
+            add(FirstValue, SecondValue)
+            break;
+
+        case '-':
+            subtract(FirstValue, SecondValue)
+            break;
+
+        case '*':
+            multiply(FirstValue, SecondValue)
+            break;
+
+        case '/':
+            divide(FirstValue, SecondValue)
+            break;
+    }
+    ActualNumericValue = result
+    FirstValue = '0' 
+    SecondValue = '0'
+    result = '0'
+}
+
+// Dark mode
+
+/*
+function ChangeDarkMode() {
+}
+*/
